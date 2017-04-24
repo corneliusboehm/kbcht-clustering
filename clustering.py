a0e5a1821ee3cb5f8eae33879bc3c8e5108bb672
crekowski <cornelius.vonrekowski@gmail.com>
2017-04-24 12:59:06 +0200

    Add main file with function templates

---
+++ clustering.py
@@ -0,0 +1,11 @@
+def einfaches_clustering(data, arg1, arg2):
+    # TODO: implement
+    list_of_labels = []
+    return list_of_labels
+
+def tolles_clustering_mit_visualisierung(data, arg1, arg2):
+    # TODO: implement
+    list_of_labels = []
+    list_of_image_filenames = []
+    return list_of_labels, list_of_image_filenames
+

a415611f2ac5701335d77cd498f9eedd89b4841c
crekowski <cornelius.vonrekowski@gmail.com>
2017-04-28 13:22:50 +0200

    Add evaluation function

--- clustering.py
+++ clustering.py
@@ -1,15 +1,25 @@
 import numpy as np
 from scipy.io.arff import loadarff
+from sklearn import metrics
 from sklearn.cluster import KMeans
 
 
+############################## utility functions ###############################
+
 def load_data(f):
-    d, meta = loadarff(f)
+    data, meta = loadarff(f)
     
-    # remove class attribute from data
-    d = np.array([e.tolist()[:-1] for e in d])
+    # split data and class attribute
+    d = np.array([e.tolist()[:-1] for e in data])
+    l = np.array([int(e.tolist()[-1]) for e in data])
     
-    return d
+    return d, l
+
+def evaluate(labels_true, labels_pred):
+    return metrics.adjusted_rand_score(labels_true, labels_pred)
+
+
+############################### KBCHT algorithm ################################
 
 def kmeans(d, k):
     km = KMeans(n_clusters=k).fit(d)
@@ -32,10 +42,13 @@ def tolles_clustering_mit_visualisierung(data, arg1, arg2):
 
 if __name__ == "__main__":
     print('Load data')
-    d = load_data('data/1-training.arff')
+    d, labels_true = load_data('data/1-training.arff')
     
     print('Perform k-means clustering')
     km = kmeans(d, 3)
+    labels_pred = km.predict(d)
+    e = evaluate(labels_true, labels_pred)
+    print('Score: {}'.format(e))
     
     print('Done')
 
